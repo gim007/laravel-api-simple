f3088161095aeac816c060a6781f8212a0bcc277
Igor <igor.g.work@gmail.com>
2024-09-02 16:13:41 +0300

    Small fix for GET request

--- routes/api.php
+++ routes/api.php
@@ -1,7 +1,11 @@
 <?php
 
 use App\Http\Controllers\SubmissionController;
+use Illuminate\Http\JsonResponse;
 
 Route::post('/submit', [SubmissionController::class, 'store']);
+Route::get('/submit', function () {
+    return response()->json(['message' => 'Please use POST request.'], 403);
+});
 
 
